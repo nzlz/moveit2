0933b668054db842e64faf762589418c20cfc2ef
Víctor Mayoral Vilches <v.mayoralv@gmail.com>
2019-04-14 11:46:27 +0200

    Fix Docker file for reproducing the issue

--- .docker/ci/docker-entrypoint.sh
+++ .docker/ci/docker-entrypoint.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-# /bin/bash -c "find /opt/ros/crystal/ -name tf2_eigen | xargs rm -rf && source /opt/ros/crystal/setup.bash \
-# 	       && touch /root/ws_moveit/src/image_common/camera_calibration_parsers/COLCON_IGNORE \
-#               && touch /root/ws_moveit/src/image_common/camera_info_manager/COLCON_IGNORE \
-#               && colcon build --merge-install"
+# # setup ros2 environment
+source "/opt/ros/crystal/setup.bash"
 
+find /opt/ros/crystal/ -name tf2_eigen | xargs rm -rf && source /opt/ros/crystal/setup.bash \
+	       && touch /opt/ws_moveit/src/image_common/camera_calibration_parsers/COLCON_IGNORE \
+              && touch /opt/ws_moveit/src/image_common/camera_info_manager/COLCON_IGNORE \
+              && colcon build --merge-install
 
-# setup ros2 environment
-source "/opt/ros/$ROS_DISTRO/setup.bash"
 exec "$@"
